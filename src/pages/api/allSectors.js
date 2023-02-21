9248f9c9b3e8f1d47c65cfd4d85ea0d095efe527
afifsylhet <afif.ahmed@yahoo.com>
2023-02-21 21:28:49 +0600

    database restore and reset for solving current problem

--- src/pages/api/allSectors.js
+++ src/pages/api/allSectors.js
@@ -1,7 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a3ykp.mongodb.net/testForAfif?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oyjsjo4.mongodb.net/?retryWrites=true&w=majority`;
 export default async function myData(req, res) {
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
